fbeb7a6b279781f73ecbf4ed04efd6e103ee4444
Leonardo A <brinfo.leo@gmail.com>
2021-11-03 18:41:20 +0000

    Adding test for the rover to cross the plateau

--- src/test/getFinalPosition.test.js
+++ src/test/getFinalPosition.test.js
@@ -11,17 +11,17 @@ test("Check if the rover received the coordinate", () => {
     expect(getFinalPosition.rover = tstRover).toBe(tstRover)
 });
 // Move to RIGHT
-test('Moving the rover on a 5x5 plater from north to east', () => {
+test('Moving the rover on a 5x5 plateau from north to east', () => {
     expect(getFinalPosition('5 5', '1 2 N', 'R')).toBe('1 2 E');
 });
-test('Moving the rover on a 5x5 plater from east to south', () => {
+test('Moving the rover on a 5x5 plateau from east to south', () => {
     expect(getFinalPosition('5 5', '1 2 E', 'R')).toBe('1 2 S');
 });
-test('Moving the rover on a 5x5 plater from south to west', () => {
+test('Moving the rover on a 5x5 plateau from south to west', () => {
     expect(getFinalPosition('5 5', '1 2 S', 'R')).toBe('1 2 W');
 
 });
-test('Moving the rover on a 5x5 plater from west to north', () => {
+test('Moving the rover on a 5x5 plateau from west to north', () => {
     expect(getFinalPosition('5 5', '1 2 W', 'R')).toBe('1 2 N');
 });
 
@@ -30,32 +30,30 @@ test('Moving the rover on a 5x5 plator in 360 degrees', () => {
 });
 
 // Move to LEFT
-test('Moving the rover on a 5x5 plater from north to west', () => {
+test('Moving the rover on a 5x5 plateau from north to west', () => {
     expect(getFinalPosition('5 5', '1 2 N', 'L')).toBe('1 2 W');
 });
-test('Moving the rover on a 5x5 plater from west to south', () => {
+test('Moving the rover on a 5x5 plateau from west to south', () => {
     expect(getFinalPosition('5 5', '1 2 W', 'L')).toBe('1 2 S');
 });
-test('Moving the rover on a 5x5 plater from south to east', () => {
+test('Moving the rover on a 5x5 plateau from south to east', () => {
     expect(getFinalPosition('5 5', '1 2 S', 'L')).toBe('1 2 E');
 
 });
-test('Moving the rover on a 5x5 plater from east to north', () => {
+test('Moving the rover on a 5x5 plateau from east to north', () => {
     expect(getFinalPosition('5 5', '1 2 E', 'L')).toBe('1 2 N');
 });
 
-test('Moving the rover on a 5x5 plator in 360 degrees', () => {
+test('Moving the rover on a 5x5 plateau in 360 degrees', () => {
     expect(getFinalPosition('5 5', '1 2 N', 'LLLL')).toBe('1 2 N');
 });
 
-
-
-
-
-test('Moving the rover simply: Test 2', () => {
-    expect(getFinalPosition('5 5', '3 3 E', 'MMRMMRMRRM')).toBe('5 1 E');
+// Crossing the plateau diagonally
+test('Moving the rover on a 10 x 10 plateau from position 00 to position 1010', () => {
+    expect(getFinalPosition('10 10', '0 0 N', 'MRMLMRMLMRMLMRMLMRMLMRMLMRMLMRMLMRMLMRML')).toBe('10 10 N');
 });
 
-test('Trying to throw the rover off the plateau', () => {
-    expect(getFinalPosition('5 5', '1 1 N', 'LMMMMM')).toBe('0 1 W');
+test('Trying to throw the rover off the 4x3 plateau', () => {
+    expect(getFinalPosition('4 3', '1 1 N', 'LMMMMM')).toBe('0 1 W');
+  
 });
