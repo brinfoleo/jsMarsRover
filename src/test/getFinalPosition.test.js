e6bfadf11b1a14123e56e53bd72c1db854c17899
Leonardo A <brinfo.leo@gmail.com>
2021-11-03 18:26:22 +0000

    Test by moving the rover to the right

--- src/test/getFinalPosition.test.js
+++ src/test/getFinalPosition.test.js
@@ -1,19 +1,35 @@
 const getFinalPosition = require("../getFinalPosition.js");
 
-test ("Check if the rover received the coordinate",()=>{
+test("Check if the rover received the coordinate", () => {
     //Arrange
-    const tstRover={
+    const tstRover = {
         x: 10,
         y: 10,
         direction: 'N'
     }
     //Act and Assert
-    expect(getFinalPosition.rover=tstRover).toBe(tstRover)
+    expect(getFinalPosition.rover = tstRover).toBe(tstRover)
 });
 
-test('Moving the rover simply: Test 1', () => {
-    expect(getFinalPosition('5 5', '1 2 N', 'LMLMLMLMM')).toBe('1 3 N');
+test('Moving the rover on a 5x5 plater from north to east', () => {
+    expect(getFinalPosition('5 5', '1 2 N', 'R')).toBe('1 2 E');
 });
+test('Moving the rover on a 5x5 plater from east to south', () => {
+    expect(getFinalPosition('5 5', '1 2 E', 'R')).toBe('1 2 S');
+});
+test('Moving the rover on a 5x5 plater from south to west', () => {
+    expect(getFinalPosition('5 5', '1 2 S', 'R')).toBe('1 2 W');
+
+});
+test('Moving the rover on a 5x5 plater from west to north', () => {
+    expect(getFinalPosition('5 5', '1 2 W', 'R')).toBe('1 2 N');
+});
+
+test('Moving the rover on a 5x5 plator in 360 degrees', () => {
+    expect(getFinalPosition('5 5', '1 2 N', 'RRRR')).toBe('1 2 N');
+});
+
+
 
 test('Moving the rover simply: Test 2', () => {
     expect(getFinalPosition('5 5', '3 3 E', 'MMRMMRMRRM')).toBe('5 1 E');
